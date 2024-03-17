732b8fb813fe43280ea2b0e7726cbd18c1f68562
siner308 <siner308@gmail.com>
2024-03-18 01:31:38 +0900

    소개글과 본식/식사시간 표기, 이미지 추가

--- src/components/PhotoGallery/index.tsx
+++ src/components/PhotoGallery/index.tsx
@@ -1,38 +1,19 @@
-import Modal from '@/components/PhotoGallery/Modal';
-import { useState } from 'react';
-import Image from 'next/image';
+'use client';
 
 const PhotoGallery = () => {
-  const [openModal, setOpenModal] = useState(false);
   const images = [
-    '/sample/150.png',
-    '/sample/200.png',
-    '/sample/300.png',
-    '/sample/400.png',
-    '/sample/500.png',
-    '/sample/600.png',
+    '/sample/400.jpeg',
+    '/sample/400-2.jpeg',
   ];
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handleOpenModal = (index: number) => {
-    setCurrentIndex(index);
-    setOpenModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setOpenModal(false);
-  };
 
   return (
-    <div className={''}>
-      <div className={'gap-4 grid justify-items-center grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5'}>
-        {images.map((image, i) => (
-          <div key={i} onClick={() => handleOpenModal(i)}>
-            <Image src={image} alt={image} width={150} height={150} />
-          </div>
-        ))}
-      </div>
-      {openModal ? <Modal index={currentIndex} images={images} onClose={handleCloseModal}/> : null}
+    <div className={'flex gap-2'}>
+      {images.map((image, i) => {
+        const imgClass = `url('${image}')`;
+        return (
+          <div style={{ backgroundImage: imgClass }} className={'bg-no-repeat bg-contain w-full h-[400px]'} key={i} />
+        );
+      })}
     </div>
   );
 };
