96c6929b3491ef0ad18a71cae0aeacc7d5e7c206
siner308 <siner308@gmail.com>
2024-03-20 01:51:58 +0900

    타임테이블 삐져나오는 이슈 해결

--- src/components/Introduce/index.tsx
+++ src/components/Introduce/index.tsx
@@ -1,28 +1,30 @@
 const Events = () => {
   return (
     <div className={'flex justify-between gap-2 place-self-center'}>
-      <div className={'w-40'}>
-        <div className={'h-20 text-end mt-[-4px]'}>12:00</div>
-        <div className={'h-20 text-end mt-[-3px]'}>12:30</div>
-        <div className={'h-20 text-end mt-[-2px]'}>13:00</div>
-        <div className={'h-20 text-end mt-[-1px]'}>13:30</div>
-      </div>
-      <div className={'w-40'}>
-        <div className={'h-20 bg-orange-100 border-white border'}></div>
-        <div className={'h-20 bg-orange-300 text-center flex flex-col justify-center'}>
-          <div>본식</div>
-          <div>12:30 - 13:00</div>
+      <div className={'w-full flex gap-2'}>
+        <div className={'mt-[-10px]'}>
+          <div className={'h-16 text-end'}>12:00</div>
+          <div className={'h-16 text-end'}>12:30</div>
+          <div className={'h-16 text-end'}>13:00</div>
+          <div className={'h-16 text-end'}>13:30</div>
+          <div className={'h-16 text-end'}>14:00</div>
         </div>
-        <div className={'h-20 bg-orange-100 border-white border'}></div>
-        <div className={'h-20 bg-orange-100 border-white border'}></div>
+        <div>
+          <div className={'w-28 h-16 bg-orange-100 border-white border'}></div>
+          <div className={'w-28 h-16 bg-orange-300 text-center flex flex-col justify-center'}>
+            <div>본식</div>
+            <div>12:30 - 13:00</div>
+          </div>
+          <div className={'w-28 h-16 bg-orange-100 border-white border'}></div>
+          <div className={'w-28 h-16 bg-orange-100 border-white border'}></div>
+        </div>
       </div>
-      <div className={'w-40'}>
-        <div className={'h-80 bg-blue-300 text-center flex flex-col justify-center'}>
+      <div className={'w-full'}>
+        <div className={'w-28 h-64 bg-blue-300 text-center flex flex-col justify-center'}>
             <div>식사</div>
             <div>12:00 - 14:00</div>
         </div>
       </div>
-      <div className={'w-40'}></div>
     </div>
   );
 };
