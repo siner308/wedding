7c6b14d1639c6bce514e880911968ced4d785bcb
siner308 <siner308@gmail.com>
2024-03-19 23:30:55 +0900

    timetable ui 개선

--- src/components/Introduce/index.tsx
+++ src/components/Introduce/index.tsx
@@ -1,14 +1,28 @@
 const Events = () => {
   return (
-    <div className={'flex justify-between gap-2'}>
-      <div className={'w-full h-20 bg-orange-300 mt-20 text-center flex flex-col justify-center'}>
-        <div>본식</div>
-        <div>12:30 - 13:00</div>
+    <div className={'flex justify-between gap-2 place-self-center'}>
+      <div className={'w-40'}>
+        <div className={'h-20 text-end mt-[-4px]'}>12:00</div>
+        <div className={'h-20 text-end mt-[-3px]'}>12:30</div>
+        <div className={'h-20 text-end mt-[-2px]'}>13:00</div>
+        <div className={'h-20 text-end mt-[-1px]'}>13:30</div>
       </div>
-      <div className={'w-full h-80 bg-blue-300 text-center flex flex-col justify-center'}>
-        <div>식사</div>
-        <div>12:00 - 14:00</div>
+      <div className={'w-40'}>
+        <div className={'h-20 bg-orange-100 border-white border'}></div>
+        <div className={'h-20 bg-orange-300 text-center flex flex-col justify-center'}>
+          <div>본식</div>
+          <div>12:30 - 13:00</div>
+        </div>
+        <div className={'h-20 bg-orange-100 border-white border'}></div>
+        <div className={'h-20 bg-orange-100 border-white border'}></div>
       </div>
+      <div className={'w-40'}>
+        <div className={'h-80 bg-blue-300 text-center flex flex-col justify-center'}>
+            <div>식사</div>
+            <div>12:00 - 14:00</div>
+        </div>
+      </div>
+      <div className={'w-40'}></div>
     </div>
   );
 };
