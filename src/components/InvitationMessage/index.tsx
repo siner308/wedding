6b8ef2a44fb60ff24fa102e9eaa814b16cf9546d
siner308 <siner308@gmail.com>
2024-03-25 18:19:13 +0900

    구글 캘린더 복사 기능 추가 및 초대메시지 변수 분리

--- src/components/InvitationMessage/index.tsx
+++ src/components/InvitationMessage/index.tsx
@@ -1,34 +1,35 @@
 import Family from '@/components/InvitationMessage/Family';
+import content from '@/app/content.json';
 
 const InvitationMessage = () => {
   const groom = {
     name: '정현',
     father: '안재흥',
-    mother: '정선순'
-  }
+    mother: '정선순',
+  };
 
   const bride = {
     name: '지원',
     father: '박창규',
-    mother: '이연화'
-  }
+    mother: '이연화',
+  };
 
   return (
     <div className={'flex flex-col gap-10 text-lg'}>
       <div className={'text-center'}>
-        <p>저희들의 오늘이 있기까지 보내주신</p>
-        <p>따뜻한 사랑과 깊은 관심에 진심으로 감사하오며,</p>
-        <p>저희 두사람은 여러분의 축복을 받으며</p>
-        <p>진실한 가약을 맺고자 합니다.</p>
-        <p>부디 참석하시어 기쁨의 자리를 축복으로</p>
-        <p>더욱 빛내 주시길 바라옵니다.</p>
+        {content.invitationMessage
+          .split('\n')
+          .map((line, index) => (
+            <p key={index}>{line}</p>
+          ))
+        }
       </div>
       <div className={'flex flex-col gap-4'}>
-        <Family father={groom.father} mother={groom.mother} gender={'male'} me={groom.name} />
-        <Family father={bride.father} mother={bride.mother} gender={'female'} me={bride.name} />
+        <Family father={groom.father} mother={groom.mother} gender={'male'} me={groom.name}/>
+        <Family father={bride.father} mother={bride.mother} gender={'female'} me={bride.name}/>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default InvitationMessage;
--- src/components/Share/index.tsx
+++ src/components/Share/index.tsx
@@ -1,7 +1,9 @@
-'use client'
+'use client';
 
 import { useEffect, useState } from 'react';
 import content from '@/app/content.json';
+import Link from 'next/link';
+import invitationMessage from '@/components/InvitationMessage';
 
 const ShareEvent = () => {
   const [clipboardCopied, setClipboardCopied] = useState(false);
@@ -27,14 +29,37 @@ const ShareEvent = () => {
       });
     }
   };
+
+  const googleCalendarBaseUrl = 'https://www.google.com/calendar/event';
+  const googleCalendarParam = new URLSearchParams({
+    location: content.addressGoogle,
+    action: 'TEMPLATE',
+    details: `${content.invitationMessage}
+
+모바일 청첩장: https://wedding.siner.io`,
+    text: content.title,
+    dates: '20240706T033000Z/20240706T050000Z',
+  });
+  const googleCalendarLink = `${googleCalendarBaseUrl}?${googleCalendarParam.toString()}`;
+
   return (
-    <div className={'px-4'}>
+    <div className={'px-4 flex flex-col gap-4'}>
       <button
         className={'text-center border bg-gray-100 text-lg h-14 w-full rounded-md'}
         onClick={handleClickShare}
       >
         {clipboardCopied ? '주소가 복사되었습니다' : '공유하기'}
       </button>
+      <Link
+        href={googleCalendarLink}
+        target={'_blank'}
+      >
+        <div
+          className={'flex justify-center items-center border bg-gray-100 text-lg h-14 rounded-md'}
+        >
+          구글 캘린더에 추가
+        </div>
+      </Link>
     </div>
   );
 };
