1c9a75d5544b917fcdb1c3608881aac23ca0c659
siner308 <siner308@gmail.com>
2024-04-06 21:42:44 +0900

    배경음악 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,14 @@ import ShareEvent from '@/components/Share';
 import SourceCodeReference from '../components/SourceCodeReference';
 import BankAccountGroup from '@/components/BankAccountGroup';
 import Background from '@/components/Background';
+import BackgroundMusic from '@/components/BackgroundMusic';
+import NoSSR from '@/components/NoSSR';
 
 export default function Home() {
   return (
-    <div><Background/>
-      <div className={'flex flex-col gap-16 mb-4'}>
+    <div>
+      <NoSSR><Background/></NoSSR>
+      <div className={'flex flex-col gap-16 mb-20'}>
         <div className={'flex flex-col gap-8 max-w-3xl mx-auto shadow-2xl'}>
           <MainBanner/>
           <InvitationMessage/>
@@ -25,8 +28,9 @@ export default function Home() {
         <div className={'max-w-3xl w-full mx-auto'}>
           <ShareEvent/>
         </div>
-        <div className={'max-w-3xl w-full mx-auto'}>
+        <div className={'max-w-3xl w-full mx-auto flex flex-col gap-4'}>
           <SourceCodeReference/>
+          <NoSSR><BackgroundMusic/></NoSSR>
         </div>
       </div>
     </div>
---
+++ src/components/BackgroundMusic/index.tsx
@@ -0,0 +1,24 @@
+export const BackgroundMusic = () => {
+  const musics = [
+    '/musics/노을의 저편에서.mp3',
+  ];
+
+  return (
+    <>
+      <div className={'fixed bottom-4 right-4'}>
+        <audio
+          controls={true}
+          autoPlay={true}
+          loop={true}
+          src={musics[0]}
+          controlsList={'nodownload noplaybackrate'}
+        />
+      </div>
+      <div className={'text-center'}>
+        음원: 노을의 저편에서 - 브금대통령
+      </div>
+    </>
+  );
+};
+
+export default BackgroundMusic;
