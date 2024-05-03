33604d82229fb7a6f91ee9e87c276271d788b617
siner308 <siner308@gmail.com>
2024-05-04 04:56:35 +0900

    이미지 추가

--- src/components/PhotoGallery/index.tsx
+++ src/components/PhotoGallery/index.tsx
@@ -1,23 +1,47 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 const PhotoGallery = () => {
+  const [width, setWidth] = useState(0);
   const images = [
-    '/images/gallery-left.jpg',
-    '/images/gallery-right.jpg',
+    { path: '/images/gallery/1.jpg', width: 266 },
+    { path: '/images/gallery/5.jpg', width: 266 },
+    { path: '/images/gallery/2.jpg', width: 266 },
+    { path: '/images/gallery/4.jpg', width: 266 },
+    { path: '/images/gallery/6.jpg', width: 266 },
+    { path: '/images/gallery/3.jpg', width: 600 },
   ];
 
+  const handleResize = () => {
+    const container = document.querySelector('#gallery');
+    if (!container) return;
+    const parentWidth = container.parentElement?.clientWidth!;
+    const windowWidth = window.outerWidth;
+    setWidth(parentWidth > windowWidth ? windowWidth : parentWidth);
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div className={'grid grid-cols-1 lg:grid-cols-2 gap-2 justify-center'}>
-      {images.map((image, i) => {
-        const imgClass = `url('${image}')`;
-        return (
-          <div
-            style={{ backgroundImage: imgClass }}
-            className={'bg-center bg-no-repeat bg-cover w-full h-[400px] pointer-events-none'}
-            key={i}
-          />
-        );
-      })}
+    <div id={'gallery'} className={'overflow-x-scroll'} style={{width: `${width}px`}}>
+      <div className={'flex gap-2'}>
+        {images.map((image, i) => {
+          const imgClass = `url('${image.path}')`;
+          const width = image.width === 266 ? 'px-[200px]' : 'px-[450px]';
+          return (
+            <div
+              style={{ backgroundImage: imgClass }}
+              className={`bg-center bg-no-repeat bg-cover ${width} h-[600px] pointer-events-none`}
+              key={i}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
