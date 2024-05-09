feef3708001b2fbf727c72b567828e539c98fd05
siner308 <siner308@gmail.com>
2024-05-09 22:40:26 +0900

    gallery 이미지 사이즈 조절

--- src/components/PhotoGallery/index.tsx
+++ src/components/PhotoGallery/index.tsx
@@ -5,12 +5,12 @@ import { useEffect, useState } from 'react';
 const PhotoGallery = () => {
   const [width, setWidth] = useState(0);
   const images = [
-    { path: '/images/gallery/1.jpg', width: 266 },
-    { path: '/images/gallery/5.jpg', width: 266 },
-    { path: '/images/gallery/2.jpg', width: 266 },
-    { path: '/images/gallery/4.jpg', width: 266 },
-    { path: '/images/gallery/6.jpg', width: 266 },
-    { path: '/images/gallery/3.jpg', width: 600 },
+    { path: '/images/gallery/1.jpg', width: 'short' },
+    { path: '/images/gallery/5.jpg', width: 'short' },
+    { path: '/images/gallery/2.jpg', width: 'short' },
+    { path: '/images/gallery/4.jpg', width: 'short' },
+    { path: '/images/gallery/6.jpg', width: 'short' },
+    { path: '/images/gallery/3.jpg', width: 'wide' },
   ];
 
   const handleResize = () => {
@@ -32,11 +32,11 @@ const PhotoGallery = () => {
       <div className={'flex gap-2'}>
         {images.map((image, i) => {
           const imgClass = `url('${image.path}')`;
-          const width = image.width === 266 ? 'px-[200px]' : 'px-[450px]';
+          const width = image.width === 'short' ? 'px-[166px]' : 'px-[375px]';
           return (
             <div
               style={{ backgroundImage: imgClass }}
-              className={`bg-center bg-no-repeat bg-cover ${width} h-[600px] pointer-events-none`}
+              className={`bg-center bg-no-repeat bg-cover ${width} h-[500px] pointer-events-none`}
               key={i}
             />
           );
