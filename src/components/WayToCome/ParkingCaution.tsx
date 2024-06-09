246a1eb0cfbbf6320bd80d18adf7b56af2539a0a
siner308 <siner308@gmail.com>
2024-06-10 02:01:42 +0900

    주차 모달 폰트 사이즈 조절

--- src/components/WayToCome/ParkingCaution.tsx
+++ src/components/WayToCome/ParkingCaution.tsx
@@ -16,14 +16,14 @@ const ParkingCautionModal = (props: Props) => {
         ' border border-black'
       }>
         <div id={'parking-caution-content'}>
-          <div className={'w-full h-32 p-2 font-bold content-center'}>
+          <div className={'w-full h-32 p-2 content-center'}>
             주차장 입구는 <u className={'decoration-[#00F802]'}>좌측의 오피스 방향</u>입니다. (아래 사진 참고)
             <br/>
             주차권은 식당을 나가면서 받으실 수 있으며, 주차는 <u>2시간 무료</u>입니다.
           </div>
           <img className={'w-full aspect-[1/1]'} src={previewImage} alt="주차장 입구 미리보기"/>
         </div>
-        <div className={'w-full h-16 text-xl py-2 cursor-pointer content-center'} onClick={onClose}>닫기</div>
+        <div className={'w-full h-16 text-lg py-2 cursor-pointer content-center'} onClick={onClose}>닫기</div>
       </div>
     ) : null
   );
